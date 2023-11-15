bc5671c886c9308c3a9241fe99b79554e305f976
FransiscusSuhargo <s160422070@student.ubaya.ac.id>
2023-11-15 23:03:22 +0700

    Gini Udah Selesai

--- Data Mining-BestSplit/Data Mining-BestSplit/ClassBestSplit.cs
+++ Data Mining-BestSplit/Data Mining-BestSplit/ClassBestSplit.cs
@@ -14,19 +14,22 @@ namespace Data_Mining_BestSplit
     public class ClassBestSplit
     {
         private DataGridView data;
+        int rowCount;
+        int columnCount;
+        List<object> listClassType;
 
         public ClassBestSplit(DataGridView data)
         {
             Data = data;
+            rowCount = Data.Rows.Count;
+            columnCount = Data.Columns.Count;
         }
 
         public DataGridView Data { get => data; set => data = value; }
 
         public void countGINI(ListBox listBox)
         {
-            int rowCount = Data.Rows.Count;
-            int columnCount = Data.Columns.Count;
-            List<object> listClassType = new List<object>();
+            listClassType = new List<object>();
 
             //mencari GINI parent 
             List<int> listClassTypeCount = new List<int>();
@@ -51,10 +54,6 @@ namespace Data_Mining_BestSplit
                     }
                 }
             }
-            for (int index = 0; index < listClassTypeCount.Count; index++)
-            {
-                listBox.Items.Add(listClassTypeCount[index]);
-            }
             int classCount = listClassTypeCount.Sum();
 
             double giniParent = 0;
@@ -64,56 +63,139 @@ namespace Data_Mining_BestSplit
                 giniParent += Math.Pow(freq, 2);
             }
             giniParent = Math.Round(1 - giniParent, 4);
+            listBox.Items.Add("Gini Parent");
             listBox.Items.Add(giniParent);
             //GINI Parent Selesei 
             //Cari GINI Feature
             List<double> listWeightedGiniFeature = new List<double>();
             for (int indexColumn = 0; indexColumn < columnCount - 1; indexColumn++)
             {
-                List<object> listFeatureType = new List<object>();
-                double weightedGiniFeature = 0;
-                for (int indexRow = 0; indexRow < rowCount - 1; indexRow++)
+                int cekNilai;
+                if (int.TryParse(Data[indexColumn, 0].Value.ToString(), out cekNilai) == true)
                 {
-                    if (!(listFeatureType.Contains(Data[indexColumn, indexRow].Value)))
+                    listBox.Items.Add("Continuous Gini Mulai");
+                    List<double> listGini = ContinuousGini();
+                    foreach(double g in listGini)
                     {
-                        listFeatureType.Add(Data[indexColumn, indexRow].Value);
+                        listBox.Items.Add(g);
                     }
+                    listBox.Items.Add("Continuous Gini Selesai");
                 }
-                foreach (object type in listFeatureType)
+                else
                 {
-                    listClassTypeCount.Clear();
-                    foreach (object classType in listClassType)
+                    List<object> listFeatureType = new List<object>();
+                    double weightedGiniFeature = 0;
+                    for (int indexRow = 0; indexRow < rowCount - 1; indexRow++)
                     {
-                        listClassTypeCount.Add(0);
-                        for (int indexRow = 0; indexRow < rowCount - 1; indexRow++)
+                        if (!(listFeatureType.Contains(Data[indexColumn, indexRow].Value)))
                         {
-                            if (classType.Equals(Data[Data.ColumnCount - 1, indexRow].Value))
-                            {
-                                listClassTypeCount[listClassType.IndexOf(classType)]++;
-                            }
+                            listFeatureType.Add(Data[indexColumn, indexRow].Value);
                         }
                     }
-                    classCount = listClassTypeCount.Sum();
-                    double giniFeature = 0;
-                    for (int index = 0; index < listClassType.Count; index++)
+                    foreach (object type in listFeatureType)
                     {
-                        double freq = listClassTypeCount[index] / (double)classCount;
-                        giniFeature += Math.Pow(freq, 2);
+                        listClassTypeCount.Clear();
+                        foreach (object classType in listClassType)
+                        {
+                            listClassTypeCount.Add(0);
+                            for (int indexRow = 0; indexRow < rowCount - 1; indexRow++)
+                            {
+                                if (classType.Equals(Data[Data.ColumnCount - 1, indexRow].Value) && Data[indexColumn, indexRow].Value.Equals(type))
+                                {
+                                    listClassTypeCount[listClassType.IndexOf(classType)]++;
+                                }
+                            }
+                        }
+                        classCount = listClassTypeCount.Sum();
+                        double giniFeature = 0;
+                        int jumlahClass = Data.Rows.Count - 1;
+                        for (int index = 0; index < listClassType.Count; index++)
+                        {
+                            double freq = listClassTypeCount[index] / (double)classCount;
+                            giniFeature += Math.Pow(freq, 2);
+                        }
+                        giniFeature = Math.Round(1 - giniFeature, 4);
+                        weightedGiniFeature += giniFeature * classCount / jumlahClass;
                     }
-                    giniFeature = Math.Round(1 - giniParent, 4);
-                    listBox.Items.Add("Gini Feature" + type);
-                    listBox.Items.Add(giniFeature);
-                    weightedGiniFeature += giniFeature * classCount/Data.Rows.Count;
+                    weightedGiniFeature = Math.Round(weightedGiniFeature, 4);
+                    listBox.Items.Add("Weighted Gini");
+                    listBox.Items.Add(weightedGiniFeature);
+                    listWeightedGiniFeature.Add(weightedGiniFeature);
                 }
-                weightedGiniFeature = Math.Round(weightedGiniFeature, 4);
-                listBox.Items.Add("Weighted Gini");
-                listBox.Items.Add(weightedGiniFeature);
-                listWeightedGiniFeature.Add(weightedGiniFeature);
+                double minGiniFeature = listWeightedGiniFeature.Min();
+                int indexMinGini = listWeightedGiniFeature.IndexOf(minGiniFeature);
+                string bestSplit = Data.Columns[indexMinGini].Name;
+                listBox.Items.Add(bestSplit);
             }
-            double minGiniFeature = listWeightedGiniFeature.Min();
-            int indexMinGini = listWeightedGiniFeature.IndexOf(minGiniFeature);
-            string bestSplit = Data.Columns[indexMinGini].Name;
-            listBox.Items.Add(bestSplit);
+        }
+        public List<double> ContinuousGini()
+        {
+            List<double> listData = new List<double>(); //list angka2 di feat
+            List<double> listDataMiddle = new List<double>(); //list angka2 setelah dibagi 2
+            int columnIndex = 1;
+            for (int i = 0; i < rowCount - 1; i++) //memasukkan ke list data
+            {
+                listData.Add((double)Data[columnIndex, i].Value);
+            }
+
+            listData.Sort();
+
+            for (int i = 0; i < listData.Count + 1; i++) //memasukkan ke listDataMiddle
+            {
+                double nilaiTengah = (listData[i] + listData[i + 1]) / 2;
+                listDataMiddle.Add(nilaiTengah);
+            }
+
+            List<double> listGini = new List<double>(); //list Gini yang didapat
+            foreach (double number in listDataMiddle)
+            {
+                List<int> itemLebihDari = new List<int>();
+                List<int> itemKurangDari = new List<int>();
+                int kurangDari = 0;
+                int lebihDari = 0;
+                for (int i = 0; i < rowCount; i++)
+                {
+                    if ((double)Data[columnIndex, i].Value <= number)
+                    {
+                        kurangDari++;
+                    }
+                    else if ((double)Data[columnIndex, i].Value > number)
+                    {
+                        lebihDari++;
+                    }
+                }
+                foreach (object type in listClassType)
+                {
+                    int inClassLebih = 0;
+                    int inClassKurang = 0;
+                    for (int i = 0; i < rowCount; i++)
+                    {
+                        if ((double)Data[columnIndex, i].Value > number && Data[columnCount - 1, i].Value == type)
+                        {
+                            inClassLebih++;
+                        }
+                        else if ((double)Data[columnIndex, i].Value <= number && Data[columnCount - 1, i].Value == type)
+                        {
+                            inClassKurang++;
+                        }
+                    }
+                    itemKurangDari.Add(inClassKurang);
+                    itemLebihDari.Add(inClassLebih);
+                }
+                double giniKecilKurang = 1;
+                foreach (int angka in itemKurangDari)
+                {
+                    giniKecilKurang -= Math.Pow((angka / kurangDari), 2);
+                }
+                double giniKecilLebih = 1;
+                foreach (int angka in itemLebihDari)
+                {
+                    giniKecilLebih -= Math.Pow((angka / lebihDari), 2);
+                }
+                double gini = (giniKecilKurang * kurangDari / (kurangDari + lebihDari)) + (giniKecilLebih * lebihDari / (kurangDari + lebihDari));
+                listGini.Add(gini);
+            }
+            return listGini;
         }
     }
 }
